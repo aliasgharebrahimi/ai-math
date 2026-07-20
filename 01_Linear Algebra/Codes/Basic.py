db7b33564891222913bdf2bfe09143914643d313
aliasghar <aliasghar.deeplearning.26@gmail.com>
2026-07-20 14:12:43 +0330

    The code has been optimized and organized.

--- 01_Linear Algebra/Codes/Basic.py
+++ 01_Linear Algebra/Codes/Basic.py
@@ -2,50 +2,43 @@ import torch
 
 device = "cpu"
 # Scalar
-scalar = torch.tensor([6.2], dtype=torch.float16, device=device, requires_grad=True)
+scalar = torch.tensor([6.2], device=device, dtype=torch.float16)  # torch.Size([1])
 
 # Vector
 # Of course, in PyTorch, row vectors and column vectors are not distinct in the way they are in mathematics.
-row_vector = torch.tensor([6.2, 5.3, 8.6], dtype=torch.float32, device=device, requires_grad=True)
+row_vector = torch.tensor([6.2, 5.3, 8.6], device=device, dtype=torch.float16) # torch.Size([3]) 
+
 column_vector = torch.tensor([7.0,
                               12.3,
-                              1.0], dtype=torch.float32, device=device, requires_grad=True)
+                              1.0], device=device, dtype=torch.float16) # torch.Size([3])
 
 # matrix
 matrix = torch.tensor([[2., 1., 6.],
                        [8., 9., 4.],
-                       [7., 1., 6.]], dtype=torch.float32, device=device, requires_grad=True)
+                       [7., 1., 6.]], device=device, dtype=torch.float16)  # torch.Size([3, 3])
 
 # tensor 3d
 tensor = torch.tensor([[[4., 4.],
                        [7., 8.]],
                        
                        [[7., 2.],
-                       [1., 1.]]])
+                       [1., 1.]]], device=device, dtype=torch.float16)  # torch.Size([2, 2, 2])
 
 # Norm
-norm1 = torch.linalg.norm(row_vector, ord=1)
-norm2 = torch.linalg.norm(row_vector, ord=2)
-normb = torch.linalg.norm(row_vector, ord=float("inf"))
+vector_norm = torch.tensor([1.0, 9.0], device=device, dtype=torch.float16)
+norm1 = torch.linalg.norm(vector_norm, ord=1) # tensor(10., grad_fn=<LinalgVectorNormBackward0>)
+norm2 = torch.linalg.norm(vector_norm, ord=2)  # tensor(9.0554, grad_fn=<LinalgVectorNormBackward0>)
+normb = torch.linalg.norm(vector_norm, ord=float("inf"))  # tensor(9., grad_fn=<LinalgVectorNormBackward0>)
 
 # Two ways to understand a Unit Vector
-u = torch.tensor([1.0, 0.0], device=device)
-v = torch.tensor([8.0, 9.0], device=device)
+u = torch.tensor([1.0, 0.0], device=device, dtype=torch.float16)
+v = torch.tensor([8.0, 9.0], device=device, dtype=torch.float16)
 
 # Method 1
-q = torch.norm(v)
-q2 = torch.norm(u)
+q = torch.norm(u) # tensor(1.)
+q2 = torch.norm(v) # tensor(12.0416)
+
 
 # Method 2
-i = torch.isclose(q, torch.tensor([1.0]))
-i2 = torch.isclose(q2, torch.tensor([1.0]))
-
-print(scalar.shape) # torch.Size([1])
-print(row_vector.shape, column_vector.shape) # torch.Size([3]) torch.Size([3])
-print(matrix.shape) # torch.Size([3, 3])
-print(tensor.shape) # torch.Size([2, 2, 2])
-print(norm1) # tensor(20.1000, grad_fn=<LinalgVectorNormBackward0>)
-print(norm2) # tensor(11.8528, grad_fn=<LinalgVectorNormBackward0>)
-print(normb) # tensor(8.6000, grad_fn=<LinalgVectorNormBackward0>)
-print(q, q2) # tensor(1.), tensor(12.0416)
-print(i, i2) # tensor([False]) tensor([True])
+i = torch.isclose(q, torch.tensor([1.0]), rtol=0.0001) # tensor([True])
+i2 = torch.isclose(q2, torch.tensor([1.0]), rtol=0.0001) # tensor([False])
