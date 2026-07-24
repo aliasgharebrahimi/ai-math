e7c186e619ee441faf3ba20a3705e05f8afef98e
aliasghar <aliasghar.deeplearning.26@gmail.com>
2026-07-24 22:02:46 +0330

    The printed results were sorted.

--- 01_Linear Algebra/Codes/Basic.py
+++ 01_Linear Algebra/Codes/Basic.py
@@ -272,7 +272,8 @@ def main():
     print("="*50)
 
     # 1. نمایش تنسورها
-    print("\n[1] Tensors & Structures:")
+    print(60*"=")
+    print("[1] Tensors & Structures:")
     s, rv, cv, m, t3 = object_BasicCodes.tensors()
     print(f" - Scalar:    {s} | Shape: {s.shape}")
     print(f" - Row Vector: {rv} | Shape: {rv.shape}")
@@ -283,14 +284,16 @@ def main():
     print(f" - 3D Tensor:  \n{t3} | Shape: {t3.shape}")
 
     # 2. نمایش نرم‌ها
-    print("\n[2] Norm Calculations:")
+    print(60 * "=")
+    print("[2] Norm Calculations:")
     n1, n2, nb = object_BasicCodes.norm()
     print(f" - L1 Norm:    {n1.item():.4f}")
     print(f" - L2 Norm:    {n2.item():.4f}")
     print(f" - L-inf Norm: {nb.item():.4f}")
 
     # 3. نمایش بردار واحد
-    print("\n[3] Unit Vector Check:")
+    print(60 * "=")
+    print("[3] Unit Vector Check:")
     q, q2, is_u, is_not_u, c = object_BasicCodes.unit_vector()
     a = object_BasicCodes.on_unit_vector
     print(f" - Vector u norm: {q.item():.2f} | Is Unit? {is_u.item()}")
@@ -298,25 +301,29 @@ def main():
     print(f" - vector {a} unit: {c}")
 
     # 4. angle 2vector
-    print("\n[4] angle 2vector:")
+    print(60 * "=")
+    print("[4] angle 2vector:")
     angle = object_BasicCodes.angle()
     print(f"angle 2vector: {angle}")
 
     # 4. gauss_equation
+    print(60 * "=")
     p, x = object_BasicCodes.gauss1()
-    print("\n[4] gauss:")
+    print("[4] gauss:")
     print(f" - matrix plugin:{p}")
     print(f" - x:{x[0]}, y:{x[1].detach()}")
 
     # Reflection
+    print(60 * "=")
     m1, m2 = object_BasicCodes.reflection()
-    print("\n[5] Reflection:")
+    print("[5] Reflection:")
     print(f" - a Reflection x:{m1}")
     print(f" - a Reflection y:{m2}")
 
     # Shear
+    print(60 * "=")
     ms1, ms2 = object_BasicCodes.shear()
-    print("\n[6] Shear:")
+    print("[6] Shear:")
     print(f" - a Shear x:{ms1}")
     print(f" - a Shear y:{ms2}")
 
